c1167e10beb5c724af46733cd83f8a0219ae74d8
liwa_lemjid <liwa.lemjid@esprit.tn>
2018-12-22 02:03:48 +0100

    New changes 22/12/2018  02:03

--- app/src/main/java/com/dev/liwa/reclamation/Profile/ProfileActivity.java
+++ app/src/main/java/com/dev/liwa/reclamation/Profile/ProfileActivity.java
@@ -1,5 +1,7 @@
 package com.dev.liwa.reclamation.Profile;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +9,12 @@ import android.support.v7.widget.ToolbarWidgetWrapper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toolbar;
 
+import com.dev.liwa.reclamation.AccountSettingActivity;
 import com.dev.liwa.reclamation.R;
 import com.dev.liwa.reclamation.Utils.ButtomNavigationHelper;
 
@@ -16,10 +22,14 @@ public class ProfileActivity extends AppCompatActivity {
     private static final String TAG="ProfileActivity";
     private static final int ACTIVITY_NUM = 4;
     BottomNavigationView navigation;
+    private ProgressBar progressBar;
+    private Context mContext = ProfileActivity.this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        progressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+        progressBar.setVisibility(View.GONE);
        // navigation = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
      //   ButtomNavigationHelper.setupButtomNavigationView(navigation);
 //        ButtomNavigationHelper.enableNavigation(ProfileActivity.this, navigation);
@@ -33,23 +43,16 @@ public class ProfileActivity extends AppCompatActivity {
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.profileToolBar);
         setSupportActionBar(toolbar);
 
-        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+        ImageView imageView = (ImageView) findViewById(R.id.profileImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Log.d(TAG,"onMenuItemClick :clicked menu item"+item);
-                switch (item.getItemId()){
-                    case R.id.profileMenu :
-                        Log.d(TAG,"onMenuItemClick : Navigation to profile preferences ");
-                }
-                return false;
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, AccountSettingActivity.class);
+                startActivity(intent);
             }
         });
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.profile_menu, menu);
-        return true ;
-    }
+
 }
